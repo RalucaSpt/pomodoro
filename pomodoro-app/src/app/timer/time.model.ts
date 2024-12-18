3d060a34590568b9f3560505843bab18aeb1d0aa
Raluca <spatarularisaraluca@gmail.com>
2024-12-18 09:19:54 +0200

    deploy on github pages

--- pomodoro-app/src/app/timer/time.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Time {
-    minutes: number;
-    seconds: number;
-    }
-
-  export const DEFAULT_SESSION_TIME = 25;
-  export const DEFAULT_BREAK_TIME = 5;
-  export const DEFAULT_SESSION_NUMBER = 4;
